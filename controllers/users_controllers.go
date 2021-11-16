9c42c0f0bc3794378e8c3efbb5c0fb624f83ed9d
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-11-16 15:55:30 +0700

    menambahkan template function users_controllers.go

--- controllers/users_controllers.go
+++ controllers/users_controllers.go
@@ -1,1 +1,19 @@
 package controllers
+
+import "github.com/labstack/echo/v4"
+
+func GetUsersControllers(c echo.Context) error {
+
+}
+
+func CreateUserController(c echo.Context) error {
+
+}
+
+func DeleteUserController(c echo.Context) error {
+
+}
+
+func UpdateUserController(c echo.Context) error {
+
+}
