cf428a97cf215a8d241f1c17affac906a2123dd4
NurilH <nurilhuda7337@gmail.com>
2021-11-16 20:24:49 +0700

    update config and main

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"project_altabe4_1/config"
+	"project_altabe4_1/routes"
+)
 
+func main() {
+	config.InitDB()
+	e := routes.New()
+
+	e.Logger.Fatal(e.Start(":8080"))
 }
