5ee136cbefb46e6f98097bdfb5f3496c4fdbc07c
Sahril Mahendra <sahrilmahendra@gmail.com>
2021-11-17 10:40:26 +0700

    menambahkan getusercontrollers

--- controllers/users_controllers.go
+++ controllers/users_controllers.go
@@ -1,9 +1,34 @@
 package controllers
 
-import "github.com/labstack/echo/v4"
+import (
+	"net/http"
+	"project_altabe4_1/lib/databases"
+	"strconv"
 
-func GetUsersControllers(c echo.Context) error {
+	"github.com/labstack/echo/v4"
+)
 
+func GetUserControllers(c echo.Context) error {
+	id := c.Param("id")
+	convId, err := strconv.Atoi(id)
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"code":    http.StatusBadRequest,
+			"message": "Bad Request",
+		})
+	}
+	user, e := databases.GetUser(convId)
+	if e != nil || user == nil {
+		return c.JSON(http.StatusBadRequest, map[string]interface{}{
+			"code":    http.StatusBadRequest,
+			"message": "Bad Request",
+		})
+	}
+	return c.JSON(http.StatusOK, map[string]interface{}{
+		"code":    http.StatusOK,
+		"message": "Successful Operation",
+		"data":    user,
+	})
 }
 
 func CreateUserController(c echo.Context) error {
