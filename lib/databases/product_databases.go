8eae711918cd57fd0df08972ffafb70e55323de9
NurilH <nurilhuda7337@gmail.com>
2021-11-17 21:18:05 +0700

    membuat fungsi create peoduct

--- lib/databases/product_databases.go
+++ lib/databases/product_databases.go
@@ -1,1 +1,13 @@
 package databases
+
+import (
+	"project_altabe4_1/config"
+	"project_altabe4_1/models"
+)
+
+func CreateProduct(user *models.Product) (interface{}, error) {
+	if err := config.DB.Create(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
