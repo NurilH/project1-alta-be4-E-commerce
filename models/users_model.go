555f34bc9f488cc75cae6b371f116fa596b6b71b
NurilH <nurilhuda7337@gmail.com>
2021-11-17 20:55:41 +0700

    menambahkan kerangka dan models product

--- models/users_model.go
+++ models/users_model.go
@@ -7,4 +7,5 @@ type Users struct {
 	Nama     string `json:"nama" form:"nama"`
 	Email    string `json:"email" form:"email"`
 	Password string `json:"password" form:"password"`
+	Product  []Product
 }
