1cb6663713999e6d9ae7315bab68a5420863cadb
Andri <m.a.juliansyah@gmail.com>
2021-11-17 09:59:04 +0700

    update users_databases

--- lib/databases/users_databases.go
+++ lib/databases/users_databases.go
@@ -1,1 +1,39 @@
 package databases
+
+import (
+	"project_altabe4_1/config"
+	"project_altabe4_1/models"
+)
+
+var user models.Users
+
+func GetUser(id int) (interface{}, error) {
+	if err := config.DB.First(&user, id).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func CreateUser(user models.Users) (interface{}, error) {
+	if err := config.DB.Save(&user).Error; err != nil {
+		return nil, err
+	}
+	return user, nil
+}
+
+func DeleteUser(id int) (interface{}, error) {
+	if err := config.DB.Where("id = ?", id).Delete(&user).Error; err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
+func UpdateUser(id int, user models.Users) (interface{}, error) {
+	if err := config.DB.Where("id = ?", id).Updates(&user).Error; err != nil {
+		return nil, err
+	}
+	config.DB.First(&user, id)
+	return user, nil
+}
