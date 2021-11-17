65660e1ef3543ab15f6aeeec749e4c4afac0b905
Andri <m.a.juliansyah@gmail.com>
2021-11-17 10:38:03 +0700

    Edit Query GetUser

--- lib/databases/users_databases.go
+++ lib/databases/users_databases.go
@@ -7,12 +7,17 @@ import (
 
 var user models.Users
 
+var SelectUser = []string{
+	user.Nama,
+	user.Email,
+}
+
 func GetUser(id int) (interface{}, error) {
-	if err := config.DB.First(&user, id).Error; err != nil {
+	if err := config.DB.Select(&SelectUser).First(&user, id).Error; err != nil {
 		return nil, err
 	}
 
-	return user, nil
+	return SelectUser, nil
 }
 
 func CreateUser(user models.Users) (interface{}, error) {
