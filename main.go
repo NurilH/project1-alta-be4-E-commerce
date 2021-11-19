2f1b83e7451cd5698d55d77044671e178dc0c4f7
NurilH <nurilhuda7337@gmail.com>
2021-11-19 21:06:35 +0700

    menambahkan log middleware

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"project_altabe4_1/config"
+	"project_altabe4_1/middlewares"
 	"project_altabe4_1/routes"
 )
 
 func main() {
 	config.InitDB()
 	e := routes.New()
-
+	middlewares.LogMiddlewares(e)
 	e.Logger.Fatal(e.Start(":8080"))
 }
