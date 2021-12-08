ec524d9997ba3d4f8690aa5ebad27fb79719a5f9
NurilH <nurilhuda7337@gmail.com>
2021-12-08 21:51:46 +0700

    main coba  https

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"net/http"
 	"project_altabe4_1/config"
 	"project_altabe4_1/middlewares"
 	"project_altabe4_1/routes"
@@ -10,8 +12,8 @@ func main() {
 	config.InitDB()
 	e := routes.New()
 	middlewares.LogMiddlewares(e)
-	e.Logger.Fatal(e.Start(":8080"))
-	// if err := e.StartTLS(":443", "server.crt", "server.key"); err != http.ErrServerClosed {
-	// 	log.Fatal(err)
-	// }
+	// e.Logger.Fatal(e.Start(":8080"))
+	if err := e.StartTLS(":443", "/home/ubuntu/server.crt", "/home/ubuntu/server.key"); err != http.ErrServerClosed {
+		log.Fatal(err)
+	}
 }
