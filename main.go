51094a36cbb7846e01e7e47a5d91e9195e34ade1
NurilH <nurilhuda7337@gmail.com>
2021-12-08 23:19:11 +0700

    main settigan awal

--- main.go
+++ main.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"log"
-	"net/http"
 	"project_altabe4_1/config"
 	"project_altabe4_1/middlewares"
 	"project_altabe4_1/routes"
@@ -12,9 +10,9 @@ func main() {
 	config.InitDB()
 	e := routes.New()
 	middlewares.LogMiddlewares(e)
-	// e.Logger.Fatal(e.Start(":8080"))
-	if err := e.StartTLS(":443", "server.crt", "server.key"); err != http.ErrServerClosed {
-		log.Fatal(err)
-	}
+	e.Logger.Fatal(e.Start(":8080"))
+	// if err := e.StartTLS(":443", "server.crt", "server.key"); err != http.ErrServerClosed {
+	// 	log.Fatal(err)
+	// }
 
 }
