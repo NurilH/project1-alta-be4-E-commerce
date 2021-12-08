df52212d272fb7265d729c1f2b6d19cc4df12377
NurilH <nurilhuda7337@gmail.com>
2021-12-08 22:26:55 +0700

    main coba  https

--- main.go
+++ main.go
@@ -16,4 +16,18 @@ func main() {
 	if err := e.StartTLS(":443", "server.crt", "server.key"); err != http.ErrServerClosed {
 		log.Fatal(err)
 	}
+	// e.AutoTLSManager.HostPolicy = autocert.HostWhitelist("<DOMAIN>")
+	// Cache certificates to avoid issues with rate limits (https://letsencrypt.org/docs/rate-limits)
+
+	// e.AutoTLSManager.Cache = autocert.DirCache("/home/ubuntu/.cache")
+	// e.Use(middleware.Recover())
+	// e.Use(middleware.Logger())
+	// e.GET("/", func(c echo.Context) error {
+	// 	return c.HTML(http.StatusOK, `
+	// 		<h1>Welcome to Echo!</h1>
+	// 		<h3>TLS certificates automatically installed from Let's Encrypt :)</h3>
+	// 	`)
+	// })
+
+	// e.Logger.Fatal(e.StartAutoTLS(":443"))
 }
